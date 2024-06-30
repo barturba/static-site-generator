cb3fc6b7615de6d6ffa38439bfd7e22a07c0b606
barturba <b@bartas.co>
2024-06-29 18:21:48 -0600

    added text node conversion function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,13 +1,68 @@
 import unittest
 
+from leafnode import LeafNode
 from textnode import TextNode
+from textnode import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold", "https://www.google.com")
-        node2 = TextNode("This is a text node", "bold", "https://www.google.com")
+        node = TextNode("This is a text node", "bold",
+                        "https://www.google.com")
+        node2 = TextNode("This is a text node", "bold",
+                         "https://www.google.com")
         self.assertEqual(node, node2)
+
+    def test_convert_invalid(self):
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(
+                TextNode("This is a bold text node", "bold_1", None)),
+
+    def test_convert_bold(self):
+        node_1 = TextNode("This is a bold text node", "bold", None)
+        node_2 = LeafNode("b", "This is a bold text node", None)
+        self.assertEqual(
+            text_node_to_html_node(node_1),
+            node_2
+        )
+
+    def test_convert_italic(self):
+        node_1 = TextNode("This is an italic text node", "italic", None)
+        node_2 = LeafNode("i", "This is an italic text node", None)
+        self.assertEqual(
+            text_node_to_html_node(node_1),
+            node_2
+        )
+
+    def test_convert_code(self):
+        node_1 = TextNode("This is a code text node", "code", None)
+        node_2 = LeafNode("code", "This is a code text node", None)
+        self.assertEqual(
+            text_node_to_html_node(node_1),
+            node_2
+        )
+
+    def test_convert_link(self):
+        node_1 = TextNode("This is a link text node",
+                          "link", "https://www.google.com")
+        node_2 = LeafNode("a", "This is a link text node", None, {
+                          "href": "https://www.google.com"})
+        self.assertEqual(
+            text_node_to_html_node(node_1),
+            node_2
+        )
+
+    def test_convert_image(self):
+        node_1 = TextNode(
+            "This is an image text node",
+            "image", "https://www.google.com"
+        )
+        node_2 = LeafNode("img", "", None, {
+                          "src": "https://www.google.com", "alt": "This is an image text node"})
+        self.assertEqual(
+            text_node_to_html_node(node_1),
+            node_2
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,40 @@
+from leafnode import LeafNode
+
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
         self.text_type = text_type
         self.url = url
-    
+
     def __eq__(self, other) -> bool:
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url 
-    
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(node):
+    match node.text_type:
+        case "text":
+            return LeafNode(None, node.text)
+        case "bold":
+            return LeafNode("b", node.text)
+        case "italic":
+            return LeafNode("i", node.text)
+        case "code":
+            return LeafNode("code", node.text)
+        case "link":
+            return LeafNode("a", node.text, None, {"href": node.url})
+        case "image":
+            return LeafNode("img", "", None, {"src": node.url, "alt": node.text})
+        case _:
+            raise ValueError("Invalid text_type")
