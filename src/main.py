442a05de792b3a69695a032aa25f6416271fa302
barturba <b@bartas.co>
2024-07-13 18:42:41 -0600

    Refactor copy_dir function for improved consistency and readability

--- src/main.py
+++ src/main.py
@@ -4,12 +4,10 @@ from textnode import TextNode
 
 
 def main():
-    # text_node = TextNode("Hello", "text", "https://www.google.com")
-    # print(text_node)
-    copy_dir("static", "public")
+    copy_static("static", "public")
 
 
-def copy_dir(source, destination):
+def copy_static(source, destination):
     for item in os.listdir(source):
         src_item = os.path.join(source, item)
         dst_item = os.path.join(destination, item)
@@ -21,7 +19,7 @@ def copy_dir(source, destination):
                 shutil.rmtree(dst_item)
             if not os.path.exists(dst_item):
                 os.mkdir(dst_item)
-            copy_dir(src_item, dst_item)
+            copy_static(src_item, dst_item)
 
 
 main()
