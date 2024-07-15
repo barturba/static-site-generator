4ffbed4f4e9fd714c06f5c721245951ad17ba78e
barturba <b@bartas.co>
2024-07-15 09:55:07 -0600

    feat: Generate HTML page from markdown content

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from markdown_blocks import markdown_to_html_node
 from textnode import TextNode
 
 
@@ -24,12 +25,67 @@ def copy_static(source, destination):
 
 def extract_title(markdown):
     # extract the title
+    lines = markdown.split("\n\n")
+    first_line = ""
+    if len(lines) > 0:
+        first_line = lines[0]
+    else:
+        first_line = markdown
+
     # if there is no title, raise an exception
-    if markdown.startswith("# "):
-        return markdown.strip("# ")
+    if first_line.startswith("# "):
+        return first_line.strip("# ")
     else:
         raise ValueError("Title doesn't exist")
     # strip leading # and any leading or trailing whitespace
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    markdown_string = ""
+    # read the markdown file
+    with open(from_path, "r") as f:
+        markdown_string = f.read()
+    # print(f"DEBUG:  markdown_string: {markdown_string}")
+
+    template_string = ""
+    # read the template file
+    with open(template_path, "r") as f:
+        template_string = f.read()
+    # print(f"DEBUG:  template_string: {template_string}")
+
+    html_output = markdown_to_html_node(markdown_string).to_html()
+    # print(f"DEBUG:  html_output: {html_output}")
+
+    # extract the title
+    title = extract_title(markdown_string)
+    # print(f"DEBUG:  title: {title}")
+
+    # replace title and content tags with actual input
+    template_string = template_string.replace("{{ Title }}", title)
+    # print(f"DEBUG:  template_string: {template_string}")
+
+    template_string = template_string.replace("{{ Content }}", html_output)
+    # print(f"DEBUG:  template_string: {template_string}")
+
+    output_file_name_parts = from_path.split(".md")
+    output_file_name = ""
+    if len(output_file_name_parts) == 2:
+        output_file_name = output_file_name_parts[0] + ".html"
+    # print(f"DEBUG:  output_file_name: {output_file_name}")
+
+    # TODO: get output dir name
+    # TODO: make all ancestor directories recursively up to the output path
+    os.makedirs(dest_path, exist_ok=True)
+
+    # TODO: write the output file out to the dest_path
+    f = open(output_file_name, "a")
+    f.write(template_string)
+    f.close()
+
+    # TODO: write the output file out to the dest_path
+
+    # os.makedirs(name, mode=0o777, exist_ok=False)¶
 
 
 main()
--- src/test_main.py
+++ src/test_main.py
@@ -1,7 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
-from main import extract_title
+from main import extract_title, generate_page
 
 
 class TestMain(unittest.TestCase):
@@ -13,6 +13,22 @@ class TestMain(unittest.TestCase):
         markdown = "This is the title"
         with self.assertRaises(ValueError):
             extract_title(markdown)
+
+    # TODO: extract title: strip whitespace
+    # TODO: extract title: multiline
+    # TODO: replace title
+    # TODO: replace content
+
+    # def test_extract_title_missing_title(self):
+    #     markdown = "This is the title"
+    #     with self.assertRaises(ValueError):
+    #         extract_title(markdown)
+
+    def test_generate_page(self):
+        from_path = "content/index.md"
+        template_path = "template.html"
+        dest_path = "public"
+        self.assertEqual(None, generate_page(from_path, template_path, dest_path))
 
 
 if __name__ == "__main__":
